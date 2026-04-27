fef33a739c9a43c264b87273ba4d8abffe75b8b5
Nicholas Jakins <nt.jakins@gmail.com>
2026-04-27 09:38:39 +0200

    Application starts

---
+++ src/test/java/com/bank/nl/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.nl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
